195f77aea08f392a3e9a0d0239bb4ccf1b469901
Edu-Fidelis <edu-fidelis@outlook.com>
2021-01-12 21:55:13 -0200

    Adicionando os primeiros arquivos .Net

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for (int i = 0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"JESUS É Bom, e sua misericórdia dura para sempre {i}");
+            }
+        }
+    }
+}
